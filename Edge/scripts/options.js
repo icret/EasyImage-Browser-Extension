9fe09a4d699954455550e271e8ce5985de06b2ed
陈双庆 <luffy@192.168.1.32>
2026-04-07 11:48:07 +0800

    chore: add .gitignore; ignore keys and system files; MV3 updates

--- Edge/scripts/options.js
+++ Edge/scripts/options.js
@@ -1,19 +1,30 @@
 // 将存储的数据输出到前台
-var options_host = localStorage.options_host
-document.getElementById('options_host').value = options_host;
-
-var options_token = localStorage.options_token
-document.getElementById('options_token').value = options_token;
+chrome.storage.local.get(["options_host", "options_token"], function (data) {
+  if (data.options_host) {
+    document.getElementById("options_host").value = data.options_host;
+  }
+  if (data.options_token) {
+    document.getElementById("options_token").value = data.options_token;
+  }
+});
 
 // 将数据存储
-document.getElementById('options_save').onclick = function () {
+document.getElementById("options_save").onclick = function () {
+  var host = document.getElementById("options_host").value;
+  var token = document.getElementById("options_token").value;
 
-    localStorage.options_host = document.getElementById('options_host').value;
-    localStorage.options_token = document.getElementById('options_token').value;
-    alert("保存成功!")
-    // window.location.href = "/pages/tuchuang.html";   // 跳转
-    window.open("about:blank", "_self").close();       // 关闭当前页
-}
+  chrome.storage.local.set(
+    {
+      options_host: host,
+      options_token: token,
+    },
+    function () {
+      alert("保存成功!");
+      // window.location.href = "/pages/tuchuang.html";   // 跳转
+      window.open("about:blank", "_self").close(); // 关闭当前页
+    },
+  );
+};
 /** 屏蔽右键菜单
 document.oncontextmenu = function () {
     return false;
